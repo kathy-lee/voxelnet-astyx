50247bd64c342206eb9b0a4e9af34eda7bf79546
kangleli <kangle.leehd@gmail.com>
2020-03-23 23:29:53 +0100

    print debug info

--- data.py
+++ data.py
@@ -80,6 +80,7 @@ class Data_helper:
       dic = {}
       if is_aug_data:
         dic = aug_data(index, os.path.join(cfg.DATA_DIR, data_d))
+        print('finish data augumentation.')
       else:
         pc = np.fromfile("%s/%06d.bin" % (pc_dir, int(index)), dtype=np.float32).reshape(-1,4) 
         if mode == "test":
@@ -115,7 +116,8 @@ class Data_helper:
                                                                                       self.anchors, 
                                                                                       cfg.DETECT_OBJECT, 
                                                                                       'lidar')
-      
+        print('finish rpn calculation.')
+
         dic["pos_equal_one_reg"] = np.concatenate(
                 [np.tile( dic["pos_equal_one"][..., [0]], 7), np.tile( dic["pos_equal_one"][..., [1]], 7)], axis=-1)[0] #we index to 0 because we added a batch dimension
         dic["pos_equal_one_sum"] = np.clip(np.sum(dic["pos_equal_one"], axis=(
