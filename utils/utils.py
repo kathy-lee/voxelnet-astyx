c5267555914b78b35bc46a8979609c6a71691323
kangleli <kangle.leehd@gmail.com>
2020-03-25 15:30:22 +0100

    print debug info

--- utils/utils.py
+++ utils/utils.py
@@ -720,7 +720,7 @@ def gt_boxes3d_to_yaw(batch_boxes, T_VELO_2_CAM):
         boxes_yaw = []
         print(f'boxes:{boxes}')
         for box in boxes:
-            print(f'box:{box}')
+            print(f'box:{box.shape},{box}')
             center_point = box[0:3]
             center_point[:-1] = 1
             print(f'center point shape:{center_point.shape},{center_point}')
